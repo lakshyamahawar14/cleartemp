750b377c71197506fd52608edda52848d7de6acc
lakshyamahawar14 <83651022+lakshyamahawar14@users.noreply.github.com>
2021-10-10 11:45:30 +0530

    optimizing code

--- delete_temp.bat
+++ delete_temp.bat
@@ -1,8 +1,5 @@
 @echo off
 if exist "C:\Windows\Temp\" rd /s /q "C:\Windows\Temp\"
-if not exist "C:\Windows\Temp\" mkdir "C:\Windows\Temp\"
 if exist "C:\Windows\Prefetch\" rd /s /q "C:\Windows\Prefetch\"
-if not exist "C:\Windows\Prefetch\" mkdir "C:\Windows\Prefetch\"
 if exist "C:\Users\Lakshya\AppData\Local\Temp\" rd /s /q "C:\Users\Lakshya\AppData\Local\Temp\"
-if not exist "C:\Users\Lakshya\AppData\Local\Temp\" rd /s /q "C:\Users\Lakshya\AppData\Local\Temp\"
 exit
