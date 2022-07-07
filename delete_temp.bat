a33eb0bb2a37a5637e31eb9aaaf91b64ebbaa267
lakshyamahawar14 <83651022+lakshyamahawar14@users.noreply.github.com>
2022-07-08 03:43:29 +0530

    Generalizing The Code
    Adding Username Field instead of hardcoding

--- delete_temp.bat
+++ delete_temp.bat
@@ -1,5 +1,5 @@
 @echo off
 if exist "C:\Windows\Temp\" rd /s /q "C:\Windows\Temp\"
 if exist "C:\Windows\Prefetch\" rd /s /q "C:\Windows\Prefetch\"
-if exist "C:\Users\*<username>*\AppData\Local\Temp\" rd /s /q "C:\Users\*<username>*\AppData\Local\Temp\"
+if exist "C:\Users\**<username>**\AppData\Local\Temp\" rd /s /q "C:\Users\**<username>**\AppData\Local\Temp\"
 exit
